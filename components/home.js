47d39f69a2a6258c962f18914c3a22ba1fe32925
abdelrahman <abdo.aly.k.m@gmail.com>
2026-02-16 01:26:11 +0200

    fin home component

---
+++ components/home.js
@@ -0,0 +1,53 @@
+"use strict";
+
+import { GetRandomMeal } from "../api.js";
+import { changeRoute } from "../main.js";
+
+export function HomeComponent() {
+  document.addEventListener("click", (event) => {
+    if (event.target && event.target.id === "random-meal") {
+      console.log("Button clicked");
+      GetRandomMeal();
+      // selectedRoute = routes["meal"];
+      changeRoute("meal");
+    }
+  });
+  const html = /*html*/ `
+          <div
+            class="hero-section container h-100dvh d-flex justify-content-center align-items-center"
+          >
+            <div class="row justify-content-center align-items-center">
+              <h1
+                class="font-family-main d-block text-align-center hero-heading"
+              >
+                Today Is My Lucky Day
+              </h1>
+              <button id="random-meal" type="button" class="position-relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-dices-icon lucide-dices"
+                >
+                  <rect width="12" height="12" x="2" y="10" rx="2" ry="2" />
+                  <path
+                    d="m17.92 14 3.5-3.5a2.24 2.24 0 0 0 0-3l-5-4.92a2.24 2.24 0 0 0-3 0L10 6"
+                  />
+                  <path d="M6 18h.01" />
+                  <path d="M10 14h.01" />
+                  <path d="M15 6h.01" />
+                  <path d="M18 9h.01" />
+                </svg>
+                Random Meal
+              </button>
+            </div>
+          </div>`;
+
+  return html;
+}
